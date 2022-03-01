439c760440412ca7ccffe9fdba319c9b8b18949e
Bahadır ERDUN <bahadirerdun@windowslive.com>
2022-03-01 19:46:08 +0300

    Auth

--- BLOGN.API/Controllers/ArticleController.cs
+++ BLOGN.API/Controllers/ArticleController.cs
@@ -8,63 +8,63 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BLOGN.API.Controllers
 {
-     [Route("api/v1/Article")]
-     //[Route("api/[controller]")]
-     [ApiController]
-     [Authorize]
-     public class ArticleController : ControllerBase
-     {
-          private readonly IArticleService _categoryService;
-          private readonly IMapper _mapper;
-          public ArticleController(IArticleService categoryService, IMapper mapper)
-          {
-               _mapper = mapper;
-               _categoryService = categoryService;
-          }
+    [Route("api/v1/Article")]
+    //[Route("api/[controller]")]
+    [ApiController]
 
-          [HttpGet]
-          public async Task<IActionResult> GetAll()
-          {
-               var articles = await _categoryService.GetAll();
-               var articlesDto = _mapper.Map<IEnumerable<ArticleDto>>(articles);
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleService _categoryService;
+        private readonly IMapper _mapper;
+        public ArticleController(IArticleService categoryService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
 
-               return Ok(articlesDto);
-          }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var articles = await _categoryService.GetAll();
+            var articlesDto = _mapper.Map<IEnumerable<ArticleDto>>(articles);
 
-          [HttpGet("{Id}")]
-          public async Task<IActionResult> Get(int Id)
-          {
-               var category = await _categoryService.Get(Id);
-               var categoryDto = _mapper.Map<ArticleDto>(category);
-               return Ok(categoryDto);
-          }
+            return Ok(articlesDto);
+        }
 
-          [HttpPost]
-          public async Task<IActionResult> Save(ArticleDto categoryDto)
-          {
-               var category = _mapper.Map<Article>(categoryDto);
-               var newArticle = await _categoryService.Add(category);
-               return Created(String.Empty, null); // _mapper.Map<ArticleDto>(newArticle));
-          }
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var category = await _categoryService.Get(Id);
+            var categoryDto = _mapper.Map<ArticleDto>(category);
+            return Ok(categoryDto);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Save(ArticleDto categoryDto)
+        {
+            var category = _mapper.Map<Article>(categoryDto);
+            var newArticle = await _categoryService.Add(category);
+            return Created(String.Empty, null); // _mapper.Map<ArticleDto>(newArticle));
+        }
+        [Authorize]
+        [HttpPatch]
+        public async Task<IActionResult> Patch(int Id, ArticleDto categoryDto)
+        {
+            if (Id == 0)
+            {
+                return BadRequest();
+            }
 
-          [HttpPatch]
-          public async Task<IActionResult> Patch(int Id, ArticleDto categoryDto)
-          {
-               if (Id == 0)
-               {
-                    return BadRequest();
-               }
-
-               var category = _mapper.Map<Article>(categoryDto);
-               _categoryService.Update(category);
-               return NoContent();
-          }
-
-          [HttpDelete("{Id}")]
-          public IActionResult Delete(int Id)
-          {
-               var entity = _categoryService.Delete(Id);
-               return NoContent();
-          }
-     }
+            var category = _mapper.Map<Article>(categoryDto);
+            _categoryService.Update(category);
+            return NoContent();
+        }
+        [Authorize]
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            var entity = _categoryService.Delete(Id);
+            return NoContent();
+        }
+    }
 }
